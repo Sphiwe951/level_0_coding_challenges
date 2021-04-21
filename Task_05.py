78ae999024c3570dcab6efc017464acd5f3f3bf7
Siphiwe Mposula <siphiwemposula07@gmail.com>
2021-04-21 12:21:05 +0200

    Task 0.5

--- Task_05.py
+++ Task_05.py
@@ -1,7 +1,6 @@
 import math
 
-def tri_area(len1, len2, len3):
-    s = 0.5*(len1+len2+len3)
-    val = s*((s-len1)*(s-len2)*(s-len3))
-    return math.sqrt(val)
-    
+def triangle_area(len1, len2, len3):
+    semi_perimeter = 0.5*(len1+len2+len3)
+    area_value = semi_perimeter*((semi_perimeter-len1)*(semi_perimeter-len2)*(semi_perimeter-len3))
+    return round(math.sqrt(area_value))
