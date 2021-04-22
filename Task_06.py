1e854c8e46fa3f58b1b1abff83be65b17a77a8b3
Siphiwe Mposula <siphiwemposula07@gmail.com>
2021-04-22 13:26:35 +0200

    Task 0.6

--- Task_06.py
+++ Task_06.py
@@ -1,9 +1,10 @@
 def maximum(*arguments):         
     size = len(arguments)
     counter = 0
-    max_variable = -2147483648
+    max_variable = arguments[0]
     while counter < size:
         if arguments[counter] > max_variable:
             max_variable = arguments[counter]
         counter +=1
+
     return max_variable
